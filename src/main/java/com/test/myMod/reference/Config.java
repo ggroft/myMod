4b0cd26654078ad231cafe0996f0815071afaafc
ggroft <ggroft@yahoo.com>
2015-02-04 19:37:06 +0200

    Merge remote-tracking branch 'origin/master' into ggroft

---
+++ src/main/java/com/test/myMod/reference/Config.java
@@ -0,0 +1,11 @@
+package com.test.myMod.reference;
+
+
+
+public class Config
+{
+    public static int CONFIG_VALUE1 = 2;
+    public static float CONFIG_VALUE2 = 0.5f;
+    public static int CONFIG_VALUE3 = 4;
+    public static float CONFIG_VALUE4 = 4.0f;
+}
